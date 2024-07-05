1719739c8240a8ed1c094514f464408dbec0ba9e
Ubuntu <ubuntu@ip-172-31-93-91.ec2.internal>
2024-07-05 16:18:55 +0000

    dockerfile added

--- my_env/bin/activate.fish
+++ my_env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ubuntu/project/django-todo/my_env"
+set -gx VIRTUAL_ENV "/home/ubuntu/project/django-todo2/my_env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
